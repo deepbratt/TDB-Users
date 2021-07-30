df014d7b864458e9e555e3852432a43a3d190da8
Azeem Haider <68206777+Azeem-Haider@users.noreply.github.com>
2021-07-30 09:03:37 +0500

    updated

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,8 +7,8 @@ const authenticate = catchAsync(async (req, res, next) => {
 	let token;
 	if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
 		token = req.headers.authorization.split(' ')[1];
-	} else if (req.cookies.jwt) {
-		token = req.cookies.jwt;
+	} else if (req.session.jwt) {
+		token = req.session.jwt;
 	}
 	if (!token) {
 		return next(new AppError(ERRORS.UNAUTHORIZED.NOT_LOGGED_IN, STATUS_CODE.UNAVAILABLE));
